45e1056a199c98a54ec8be1d934242ab2ef45fda
Joel Sedillo <137210980+Koda53@users.noreply.github.com>
2025-11-20 18:08:53 -0800

    Change background color to #4EBAC4 in vga_bitchange
    Updated the VGA module to change the background color and improve code readability.

--- vga_bitchange.v
+++ vga_bitchange.v
@@ -5,21 +5,8 @@
 // 
 // Create Date:    12:15:38 12/14/2017 
 // Design Name: 
-// Module Name:    vgaBitChange 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-// Date: 04/04/2020
-// Author: Yue (Julien) Niu
-// Description: Port from NEXYS3 to NEXYS4
+// Module Name:    vga_bitchange 
+// Description: Updated background color to #4EBAC4
 //////////////////////////////////////////////////////////////////////////////////
 module vga_bitchange(
 	input clk,
@@ -34,7 +21,9 @@ module vga_bitchange(
 	parameter WHITE = 12'b1111_1111_1111;
 	parameter RED   = 12'b1111_0000_0000;
 	parameter GREEN = 12'b0000_1111_0000;
-	//parameter BLUE = 12'b0000_0000_1111;
+
+	// New background color (#4EBAC4 converted to 12-bit RGB ≈ 5C C)
+	parameter BG = 12'b0101_1100_1100;  
 
 	wire whiteZone;
 	wire greenMiddleSquare;
@@ -48,46 +37,48 @@ module vga_bitchange(
 		reset = 1'b0;
 	end
 	
-	
-	always@ (*) // paint a white box on a red background
-    	if (~bright)
-		rgb = BLACK; // force black if not bright
-	 else if (greenMiddleSquare == 1)
-		rgb = GREEN;
-	 else if (whiteZone == 1)
-		rgb = WHITE; // white box
-	 else
-		rgb = RED; // background color
+	// VGA pixel coloring logic
+	always @(*) 
+		if (~bright)
+			rgb = BLACK;
+		else if (greenMiddleSquare)
+			rgb = GREEN;
+		else if (whiteZone)
+			rgb = WHITE;
+		else
+			rgb = BG; // Updated background color
 
 	
-	always@ (posedge clk)
-		begin
+	// Square movement logic
+	always @(posedge clk)
+	begin
 		greenMiddleSquareSpeed = greenMiddleSquareSpeed + 50'd1;
-		if (greenMiddleSquareSpeed >= 50'd500000) //500 thousand
-			begin
+		if (greenMiddleSquareSpeed >= 50'd500000)
+		begin
 			greenMiddleSquareY = greenMiddleSquareY + 10'd1;
 			greenMiddleSquareSpeed = 50'd0;
+			
 			if (greenMiddleSquareY == 10'd779)
-				begin
 				greenMiddleSquareY = 10'd0;
-				end
-			end
 		end
+	end
 
-	always@ (posedge clk)
-		if ((reset == 1'b0) && (button == 1'b1) && (hCount >= 10'd144) && (hCount <= 10'd784) && (greenMiddleSquareY >= 10'd400) && (greenMiddleSquareY <= 10'd475))
-			begin
+	// Collision detection / scoring
+	always @(posedge clk)
+		if ((reset == 1'b0) && (button == 1'b1) && 
+			(hCount >= 10'd144) && (hCount <= 10'd784) &&
+			(greenMiddleSquareY >= 10'd400) && (greenMiddleSquareY <= 10'd475))
+		begin
 			score = score + 16'd1;
 			reset = 1'b1;
-			end
+		end
 		else if (greenMiddleSquareY <= 10'd20)
-			begin
 			reset = 1'b0;
-			end
 
-	assign whiteZone = ((hCount >= 10'd144) && (hCount <= 10'd784)) && ((vCount >= 10'd400) && (vCount <= 10'd475)) ? 1 : 0;
+	assign whiteZone = ((hCount >= 10'd144) && (hCount <= 10'd784)) &&
+					   ((vCount >= 10'd400) && (vCount <= 10'd475));
 
 	assign greenMiddleSquare = ((hCount >= 10'd340) && (hCount < 10'd380)) &&
-				   ((vCount >= greenMiddleSquareY) && (vCount <= greenMiddleSquareY + 10'd40)) ? 1 : 0;
+							   ((vCount >= greenMiddleSquareY) && (vCount <= greenMiddleSquareY + 10'd40));
 	
 endmodule
